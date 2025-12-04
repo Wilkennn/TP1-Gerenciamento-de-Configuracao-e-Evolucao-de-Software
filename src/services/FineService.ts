8c4fce5a18581c2527ac5df048dea3ba77d8714c
Wilken <wilken.henrique2513@gmail.com>
2025-12-03 21:11:09 -0300

    feat: teste

--- src/services/FineService.ts
+++ src/services/FineService.ts
@@ -2,7 +2,7 @@ export class FineService {
   // Método simples para calcular multa baseada em dias
   calculate(days: number): number {
     // BUG: Estamos cobrando apenas 1 real por dia, mas a regra é 2
-    const valorPorDia = 1; 
+    const valorPorDia = 2; 
     return days * valorPorDia;
   }
 }
